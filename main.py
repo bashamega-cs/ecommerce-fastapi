2ff8a11c068a3b0bd38e6d08dbf5b72fee1a6f43
Bashamega <adambashaahmednaji@gmail.com>
2026-03-07 09:26:08 +0200

    Authentication

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from models import Base
 from database import engine
 from routers import items
+from routers import users
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(items.router)
+app.include_router(users.router)
 
 @app.get("/")
 def root():
--- models.py
+++ models.py
@@ -1,5 +1,24 @@
 from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy.orm import relationship
+from pydantic import BaseModel
 from database import Base
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class LoginBase(BaseModel):
+    email:str
+    password:str
+  
+class User(Base):
+    __tablename__ = "users"
+    id = Column[int](Integer, primary_key=True, index=True)
+    name = Column[str](String, index=True)
+    email = Column[str](String, unique=True, index=True)
+    hashed = Column[str](String, unique=True, index=False)
+
 
 class Item(Base):
     __tablename__ = "items"
