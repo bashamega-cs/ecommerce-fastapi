8c6e40ebc412e8626bdb50939e5e3e601a4cca71
Bashamega <adambashaahmednaji@gmail.com>
2026-03-13 09:38:26 +0200

    Integrate cors

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ from database import engine
 from routers import items
 from routers import users
 from routers import cart
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or your frontend URL
+    allow_methods=["*"],  # GET, POST, OPTIONS, etc.
+    allow_headers=["*"],  # Content-Type, Authorization, etc.
+)
 
 app.include_router(items.router)
 app.include_router(users.router)
